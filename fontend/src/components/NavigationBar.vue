ca0ddcc4f762ea276f74de2ccc282a6e30287e2a
Apisara Ngakor <apisara.99@mail.kmutt.ac.th>
2021-04-25 15:08:25 +0700

    [UPDATE]103 update NavigationBar

--- fontend/src/components/NavigationBar.vue
+++ fontend/src/components/NavigationBar.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="col-start-2 col-end-2 justify-self-center space-x-44">
-    <router-link to="/">Home</router-link>
-    <router-link to="/products">Products</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/team">Team</router-link>
+  <div class="flex justify-center">
+    <div>
+      <div class="py-6 flex justify-center">
+        <p>JW PEI</p>
+      </div>
+      <div class="space-x-44 pb-4 text-white ">
+        <router-link to="/" class="hover:text-yellow-400">Home</router-link>
+        <router-link to="/products" class="hover:text-yellow-400">Products</router-link>
+        <router-link to="/about" class="hover:text-yellow-400">About</router-link>
+        <router-link to="/team" class="hover:text-yellow-400">Team</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-* {
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Mitr:wght@200&display=swap");
+
+div {
   font-size: 20px;
-  font-family: monospace;
+  font-family: "Mitr", sans-serif;
   text-decoration: none;
-  background-color: #83c5be;
-  color: #006d77;
+  background-color: #414535;
+}
+p {
+  font-family: "Bebas Neue", sans-serif;
+  font-size: 36px;
+  color: white;
 }
 </style>
